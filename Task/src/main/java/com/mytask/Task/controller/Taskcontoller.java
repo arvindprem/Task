935cb77416ef37b7b29409e3020f440ade247b2d
Arvind.Premanandham <Arvind.Premanandham@U08869.ipsosgroup.ipsos.com>
2018-08-21 16:51:51 +0200

    created get,put,post,delete but need to rectify the errors

--- Task/src/main/java/com/mytask/Task/controller/Taskcontoller.java
+++ Task/src/main/java/com/mytask/Task/controller/Taskcontoller.java
@@ -4,26 +4,59 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mytask.Task.model.PoopyUser;
+import com.mytask.Task.model.PoppyUser;
 import com.mytask.Task.repository.Taskrepository;
 @RestController
-@RequestMapping("")
+@RequestMapping("/Poppy client database")
 public class Taskcontoller {
 	@Autowired
-	private Taskrepository repository;
+	private Taskrepository Taskrepository;
 	
-	@RequestMapping(value="",method=RequestMethod.GET)
+	@RequestMapping(value="/Poppy client database",method=RequestMethod.GET)
 	public List<User> getallPoppyuser() 
 	{
-		return repository.findAll();
+		return Taskrepository.findAll();
 	}
+	
 
-	@RequestMapping(value="/{id}" ,method= RequestMethod.GET)
-	public  PoopyUser getPoppyUserbyid(@PathVariable("id") String id) {
-	return (PoopyUser) repository .findByid(id);
+	@RequestMapping(method=RequestMethod.GET, value="/Poppy client database/{id}")
+    public PoppyUser show(@PathVariable String id) {
+        return Taskrepository.findone( id) {
+		}
+    
+
+	@RequestMapping(method=RequestMethod.POST, value="/Poppy client database")
+    public PoppyUser save(@RequestBody PoppyUser user) {
+        Taskrepository.save(user);
+
+        return user;
+    }
+	@RequestMapping(method=RequestMethod.PUT, value="/Poppy client database/{id}")
+    public void update(@PathVariable("id") String id, @RequestBody PoppyUser user)  {
+        PoppyUser p = Taskrepository.findone(id);
+        if(user.getName() != null)
+            p.setName(user.getName());
+        if(user.getLast_name() != null)
+            p.setLast_name(user.getLast_name());
+        if(user.getExpiration_date() != null)
+            p.setExpiration_date(user.getExpiration_date());
+        
+        Taskrepository.save(p);
+        return user;
+    }
+
+	@RequestMapping(method=RequestMethod.DELETE, value="/Poppy client database/{id}")
+    public void delete(@PathVariable("id") String id) {
+      PoppyUser user = Taskrepository.findone(id);
+        Taskrepository.delete(user);
+
+        return "";
+    }
 }
 }
