f624f0d8a1b0f1e2055516cd682411fc1e5d1a5c
Arvind.Premanandham <Arvind.Premanandham@U08869.ipsosgroup.ipsos.com>
2018-08-29 11:03:38 +0200

    added user for spring security

---
+++ Task/src/main/java/com/mytask/Task/model/User.java
@@ -0,0 +1,34 @@
+package com.mytask.Task.model;
+
+
+
+import java.util.Set;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+
+
+@Document(collection = "user")
+public class User {
+	
+	@Id
+	private String id;
+	@Indexed(unique = true, direction = IndexDirection.DESCENDING, dropDups = true)
+	private String email;
+	private String password;
+	private String fullname;
+	private boolean enabled;
+	@DBRef
+	private Set<Role> roles;
+
+}
