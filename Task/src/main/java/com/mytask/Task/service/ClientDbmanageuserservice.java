8a9bae9ffe9a2ecaa39e8a37f6a2249e4b84fae8
Arvind.Premanandham <Arvind.Premanandham@U08869.ipsosgroup.ipsos.com>
2018-08-29 11:06:08 +0200

    database for service

--- Task/src/main/java/com/mytask/Task/service/ClientDbmanageuserservice.java
+++ Task/src/main/java/com/mytask/Task/service/ClientDbmanageuserservice.java
@@ -4,25 +4,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
+
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
+
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import com.mytask.Task.model.PoppyUser;
+import com.mytask.Task.model.User;
 import com.mytask.Task.model.Role;
 import com.mytask.Task.repository.RoleRepository;
 import com.mytask.Task.repository.UserRepository;
 
 @Service
-public class ClientDbmanageuserservice implements PoppyUserdetailservice {
+public class ClientDbmanageuserservice implements UserDetailsService {
 	@Autowired
 	private UserRepository userRepository;
 	@Autowired
@@ -30,28 +30,28 @@ public class ClientDbmanageuserservice implements PoppyUserdetailservice {
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	
-	public PoppyUser findPoppyUserByEmail(String email) {
+	public User findUserByEmail(String email) {
 	    return userRepository.findByEmail(email);
 	}
-	public void saveUser(PoppyUser user) {
+	public void saveUser(User user) {
 	    user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 	    user.setEnabled(true);
 	    Role userRole = roleRepository.findbyrole("ADMIN");
 	    user.setRoles(new HashSet<>(Arrays.asList(userRole)));
 	    userRepository.save(user);
 	}
-	public UserDetails loadPoppyUserByUsername(String email) throws UsernameNotFoundException {
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
-	    PoppyUser user = userRepository.findByEmail(email);
+	    User user = userRepository.findByEmail(email);
 	    if(user != null) {
-	        List<GrantedAuthority> authorities = getPoppyUserAuthority(user.getRoles());
+	        List<GrantedAuthority> authorities = getUserAuthority(user.getRoles());
 	        return buildUserForAuthentication(user, authorities);
 	    } else {
 	        throw new UsernameNotFoundException("username not found");
 	    }
 	    }
 	    
-		private List<GrantedAuthority> getPoppyUserAuthority(Set<Role> userRoles) {
+		private List<GrantedAuthority> getUserAuthority(Set<Role> userRoles) {
 	        Set<GrantedAuthority> roles = new HashSet<>();
 	        userRoles.forEach((role) -> {
 	            roles.add(new SimpleGrantedAuthority(role.getRole()));
@@ -60,7 +60,7 @@ public class ClientDbmanageuserservice implements PoppyUserdetailservice {
 	        List<GrantedAuthority> grantedAuthorities = new ArrayList<>(roles);
 	        return grantedAuthorities;
 	    }
-		private UserDetails buildUserForAuthentication(PoppyUser user, List<GrantedAuthority> authorities) {
+		private UserDetails buildUserForAuthentication(User user, List<GrantedAuthority> authorities) {
 		    return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
 		}
 }
